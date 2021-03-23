b6b7095e19dbe416eea5d7102fe3de8785fa460c
LadyViktoria <LadyViktoria@users.noreply.github.com>
2021-03-23 20:34:57 +0100

    Update debian10setup.sh

--- debian10setup.sh
+++ debian10setup.sh
@@ -1,8 +1,11 @@
-sudo modprobe overlay
-sudo modprobe br_netfilter
 disable swap
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 sudo swapoff -a
+
+sudo tee /etc/modules-load.d/kubernetes.conf<<EOF
+br_netfilter
+overlay
+EOF
 
 sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
